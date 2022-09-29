a9a6baac559db21d407272bd47044400dcb9d7c7
reyhaneterzioglu <terzioglureyhane@gmail.com>
2022-09-29 17:52:18 +0200

    ticketing project ui

---
+++ src/test/java/com/cydeo/ticketing_project_mvc/TicketingProjectMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.ticketing_project_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingProjectMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
